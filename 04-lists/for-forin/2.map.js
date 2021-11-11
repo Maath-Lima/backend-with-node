7c0ac7d79b264e921e78bb32d53c3025ecc8537c
unknown <math3226@gmail.com>
2021-11-11 20:33:09 -0300

    map exemplos e criação de um tipo de map no prototype

---
+++ 04-lists/for-forin/2.map.js
@@ -0,0 +1,38 @@
+const service = require('./service')
+
+Array.prototype.meuMap = function (callback) {
+  const novoArrayMapeado = [];
+
+  for (let index = 0; index < this.length; index++) {
+    const resultado = callback(this[index], index);
+
+    novoArrayMapeado.push(resultado);
+  }
+
+  return novoArrayMapeado;
+}
+
+async function main() {
+  try {
+    const { results } = await service.obterPessoas('a');
+    // const names = [];
+
+    // results.forEach((item) => {
+    //   names.push(item.name);
+    // });
+
+    // const names = results.map((pessoa) => {
+    //                 return pessoa.name
+    //               });
+
+    const names = results.meuMap((pessoa, index) => {
+      return `${index}|${pessoa.name}` 
+    })
+
+    console.log(names);
+  } catch (error) {
+    console.error('DEU RUIM', error)
+  }
+}
+
+main();
