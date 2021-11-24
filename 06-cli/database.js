8dc92983126ed232a64fc9f1443180cc0efb42aa
Matheus Santana <matheus.santana@nydus.com.br>
2021-11-24 20:45:52 -0300

    Teste para criação de um herói e implementação dessa feature

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -1,7 +1,8 @@
-const { readFile } = require('fs')
+const { readFile, writeFile } = require('fs')
 const { promisify } = require('util')
 
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 // outra forma de obter dados json
 // const dadosJson = require('./herois.json')
@@ -17,8 +18,22 @@ class Database {
     return JSON.parse(arquivo.toString()); 
   }
 
-  escreverArquivo() {
+  async escreverArquivo(data) {
+    await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(data));
+    return true
+  }
 
+  async cadastrar(heroi) {
+    const dados = await this.obterDadosArquivo();
+    const id = heroi.id <= 2 ? heroi.id : Date.now()
+    const heroiComId = {
+      id,
+      ...heroi
+    }
+    const dadosFinal = [...dados, heroiComId];
+    const resultado = await this.escreverArquivo(dadosFinal);
+
+    return resultado;
   }
 
   async listar(id) {
